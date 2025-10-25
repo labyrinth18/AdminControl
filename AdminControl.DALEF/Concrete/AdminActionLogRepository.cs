de7d31b8d35a1020be5de67b1b5f20b140699e51
labyrinth18 <146131866+labyrinth18@users.noreply.github.com>
2025-10-25 16:58:12 +0300

    Update AdminActionLogRepository.cs
    Removed

--- AdminControl.DALEF/Concrete/AdminActionLogRepository.cs
+++ AdminControl.DALEF/Concrete/AdminActionLogRepository.cs
@@ -4,10 +4,6 @@ namespace AdminControl.DALEF.Concrete
 {
     public class AdminActionLogRepository : IAdminActionLogRepository
     {
-        // Можна додати конструктор для DbContext
-        // private readonly AdminControlContext _context;
-        // public AdminActionLogRepository(AdminControlContext context) => _context = context;
-
-        // Поки що клас порожній
+       
     }
 }
